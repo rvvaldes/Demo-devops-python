aa4673a52ed35f46cd1dd5e626be385c0fff7af0
Rodney Velazquez <rvelazquezv@deloitte.com>
2024-02-27 22:34:04 -0300

    adding flake8 to the static code test

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
