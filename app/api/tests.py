3ec6bc655100f614bcc6c567b23e2aa0919c244f
Rodney Velazquez <rvelazquezv@deloitte.com>
2024-02-27 22:26:07 -0300

    adding flake8 to the static code test

--- app/api/tests.py
+++ app/api/tests.py
@@ -1,19 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import json
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from .models import User
+from models import User
+
 
 class TestUserView(APITestCase):
+
     def setUp(self):
         user = User(name='Test1', dni='09876543210')
         user.save()
-        self.url = reverse("users-list")
+        self.url = reverse('users-list')
         self.data = {'name': 'Test2', 'dni': '09876543211'}
 
     def test_post(self):
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(json.loads(response.content), {"id": 2, "name":"Test2", "dni":"09876543211"})
+        self.assertEqual(json.loads(response.content), {'id': 2,
+                         'name': 'Test2', 'dni': '09876543211'})
         self.assertEqual(User.objects.count(), 2)
 
     def test_get_list(self):
@@ -24,4 +29,5 @@ class TestUserView(APITestCase):
     def test_get(self):
         response = self.client.get(self.url + '1/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), {"id": 1, "name":"Test1", "dni":"09876543210"})
+        self.assertEqual(json.loads(response.content), {'id': 1,
+                         'name': 'Test1', 'dni': '09876543210'})
